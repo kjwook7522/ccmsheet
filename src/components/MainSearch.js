4339328300ed7b4fb81854335df202a957d0c5d4
mona <kjwook7522@gmail.com>
2020-06-28 23:54:48 +0900

    change input style

--- src/components/MainSearch.js
+++ src/components/MainSearch.js
@@ -12,9 +12,11 @@ function MainSearch() {
 
   return (
     <div className="main">
-      <h1>이번주 찬양을 검색해보세요.</h1>
+      {/* <h1>이번주 찬양을 검색해보세요.</h1> */}
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} />
+        <span class="bar"></span>
+        <label>이번주 찬양을 검색해보세요.</label>
         <button><SearchOutlined style={{color: '#eee', fontSize: '30px'}} /></button>
       </form>
     </div>
